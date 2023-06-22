4e8d7bcb425d4353e86f4fdca4afbc54fb54a319
hakugakusha <87151999+hakugakusha@users.noreply.github.com>
2023-06-22 06:29:26 -0400

    combined tests due to platformstartup

--- src/test/java/cs3500/pa05/model/ThemeWeekTest.java
+++ src/test/java/cs3500/pa05/model/ThemeWeekTest.java
@@ -6,27 +6,37 @@ import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 import javafx.application.Platform;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.property.StringProperty;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.scene.image.Image;
 import javafx.scene.paint.Color;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 /**
- * tests for the week class
+ * Tests for the Theme class.
  */
-public class WeekTest {
+public class ThemeWeekTest {
+
+  private Theme theme;
 
   private Week week2;
   private Task task1;
-  private Theme theme;
+  private Theme theme1;
   private Theme theme2;
 
+  /**
+   * creates the days
+   *
+   * @return an Array of days
+   */
   private Day[] createDays() {
     Day[] days = new Day[7];
     DayEnum[] dayEnums = new DayEnum[] {DayEnum.SUNDAY, DayEnum.MONDAY, DayEnum.TUESDAY,
@@ -56,9 +66,9 @@ public class WeekTest {
     Day[] days = createDays();
     task1 = new Task("Finish Testing", DayEnum.SUNDAY);
     days[0].addTask(task1);
-    theme = new Theme(Color.AQUA, Color.GREEN, "Times New Roman", new ArrayList<>());
+    theme1 = new Theme(Color.AQUA, Color.GREEN, "Times New Roman", new ArrayList<>());
     theme2 = new Theme(Color.RED, Color.GREEN, "Arial", new ArrayList<>());
-    week2 = new Week("Worst Week", days, new ArrayList<>(Arrays.asList(theme, theme2)),
+    week2 = new Week("Worst Week", days, new ArrayList<>(Arrays.asList(theme1, theme2)),
         0, "hello", 1, 1);
   }
 
@@ -155,7 +165,7 @@ public class WeekTest {
    */
   @Test
   public void testGetTheme() {
-    assertEquals(week2.getTheme(), theme);
+    assertEquals(week2.getTheme(), theme1);
   }
 
   /**
@@ -179,7 +189,7 @@ public class WeekTest {
    */
   @Test
   public void testGetThemes() {
-    assertEquals(week2.getThemes(), new ArrayList<>(Arrays.asList(theme, theme2)));
+    assertEquals(week2.getThemes(), new ArrayList<>(Arrays.asList(theme1, theme2)));
   }
 
   /**
@@ -205,7 +215,7 @@ public class WeekTest {
    */
   @Test
   public void testUpdateTheme() {
-    assertEquals(week2.getTheme(), theme);
+    assertEquals(week2.getTheme(), theme1);
     week2.updateTheme(1);
     assertEquals(week2.getTheme(), theme2);
   }
@@ -227,7 +237,7 @@ public class WeekTest {
   public void testUpdateWeek() {
     Day[] newDays = createDays();
     Week week3 = new Week("ITS LIT", newDays,
-        new ArrayList<>(Arrays.asList(theme)), 0, "NEW WEEK", 15, 15);
+        new ArrayList<>(Collections.singletonList(theme)), 0, "NEW WEEK", 15, 15);
     week2.update(week3);
     System.out.println(week2.getTitle());
     assertEquals(week2.getTitle(), "ITS LIT");
@@ -238,11 +248,11 @@ public class WeekTest {
     assertEquals(week2.getDay(4), newDays[4]);
     assertEquals(week2.getDay(5), newDays[5]);
     assertEquals(week2.getDay(6), newDays[6]);
-    assertEquals(week2.getThemes(),week3.getThemes());
-    assertEquals(week3.getCurrentTheme(), week3.getCurrentTheme());
-    assertEquals(week3.getNotes(), week3.getNotes());
-    assertEquals(week3.getMaxTasks(), week3.getMaxTasks());
-    assertEquals(week3.getMaxEvents(), week3.getMaxEvents());
+    assertEquals(week2.getThemes(), week3.getThemes());
+    assertEquals(week2.getCurrentTheme(), week3.getCurrentTheme());
+    assertEquals(week2.getNotes(), week3.getNotes());
+    assertEquals(week2.getMaxTasks(), week3.getMaxTasks());
+    assertEquals(week2.getMaxEvents(), week3.getMaxEvents());
   }
 
   /**
@@ -298,5 +308,170 @@ public class WeekTest {
     assertEquals(week2.getNotes(), "");
     assertEquals(week2.getDay(0), new Day(DayEnum.SUNDAY));
   }
+
+
+  /**
+   * setup for the tests.
+   */
+  @BeforeEach
+  public void setUp() {
+    Color backgroundColor = Color.rgb(255, 255, 255);
+    Color fontColor = Color.rgb(0, 0, 0);
+    String fontFamily = "Arial";
+    List<Image> images = new ArrayList<>();
+    theme = new Theme(backgroundColor, fontColor, fontFamily, images);
+  }
+
+  @Test
+  public void testGetBackgroundColor() {
+    Color backgroundColor = theme.getBackgroundColor();
+    assertEquals(Color.rgb(255, 255, 255), backgroundColor);
+  }
+
+  @Test
+  public void testGetFontColor() {
+    Color fontColor = theme.getFontColor();
+    assertEquals(Color.rgb(0, 0, 0), fontColor);
+  }
+
+  @Test
+  public void testGetFontFamily() {
+    String fontFamily = theme.getFontFamily();
+    assertEquals("Arial", fontFamily);
+  }
+
+
+  @Test
+  public void testTheme1() {
+    Theme.ThemeBuilder builder = new Theme.ThemeBuilder();
+    Theme theme = builder.theme1().build();
+
+    Assertions.assertEquals(Color.rgb(255, 255, 255), theme.getBackgroundColor());
+    Assertions.assertEquals(Color.rgb(0, 0, 0), theme.getFontColor());
+    Assertions.assertEquals("Arial", theme.getFontFamily());
+    //Assertions.assertEquals(3, theme.getImages().size());
+    // Add more assertions if needed
+  }
+
+  @Test
+  public void testTheme2() {
+    Theme.ThemeBuilder builder = new Theme.ThemeBuilder();
+    Theme theme = builder.theme2().build();
+
+    Assertions.assertEquals(Color.rgb(250, 218, 221), theme.getBackgroundColor());
+    Assertions.assertEquals(Color.rgb(255, 0, 0), theme.getFontColor());
+    Assertions.assertEquals("Lucida Calligraphy", theme.getFontFamily());
+    //Assertions.assertEquals(3, theme.getImages().size());
+    // Add more assertions if needed
+  }
+
+  @Test
+  public void testTheme3() {
+    Theme.ThemeBuilder builder = new Theme.ThemeBuilder();
+    Theme theme = builder.theme3().build();
+
+    Assertions.assertEquals(Color.rgb(0, 0, 0), theme.getBackgroundColor());
+    Assertions.assertEquals(Color.rgb(255, 255, 255), theme.getFontColor());
+    Assertions.assertEquals("Jokerman", theme.getFontFamily());
+    //Assertions.assertEquals(3, theme.getImages().size());
+    // Add more assertions if needed
+  }
+
+  @Test
+  public void testDefaultTheme() {
+    Theme.ThemeBuilder builder = new Theme.ThemeBuilder();
+    List<Theme> themes = builder.defaultTheme();
+
+    Assertions.assertEquals(3, themes.size());
+    // Add assertions to verify the properties of each theme
+  }
+
+  @Test
+  public void testSetBackgroundColor() {
+    Theme.ThemeBuilder builder = new Theme.ThemeBuilder();
+    Color backgroundColor = Color.rgb(123, 45, 67);
+    Theme theme = builder.setBackgroundColor(backgroundColor).build();
+
+    Assertions.assertEquals(backgroundColor, theme.getBackgroundColor());
+  }
+
+  @Test
+  public void testSetFontColor() {
+    Theme.ThemeBuilder builder = new Theme.ThemeBuilder();
+    Color fontColor = Color.rgb(12, 34, 56);
+    Theme theme = builder.setFontColor(fontColor).build();
+
+    Assertions.assertEquals(fontColor, theme.getFontColor());
+  }
+
+  @Test
+  public void testSetFontFamily() {
+    Theme.ThemeBuilder builder = new Theme.ThemeBuilder();
+    String fontFamily = "Verdana";
+    Theme theme = builder.setFontFamily(fontFamily).build();
+
+    Assertions.assertEquals(fontFamily, theme.getFontFamily());
+  }
+
+  @Test
+  public void testSetImages() {
+    Platform.runLater(() -> {
+      Theme.ThemeBuilder builder = new Theme.ThemeBuilder();
+      List<Image> images = List.of(new Image("L.png"), new Image("Loveball.png"));
+      Theme theme = builder.setImages(images).build();
+
+      Assertions.assertEquals(images, theme.getImages());
+    });
+  }
+
+  @Test
+  public void testGetImages() {
+    // Create a list of images
+    List<Image> images = new ArrayList<>();
+    images.add(new Image("L.png"));
+    images.add(new Image("Loveball.png"));
+
+    // Create a theme with the list of images
+    Theme theme = new Theme(Color.WHITE, Color.BLACK, "Arial", images);
+
+    // Retrieve the images from the theme
+    List<Image> retrievedImages = theme.getImages();
+
+    // Assert that the retrieved images match the original images
+    Assertions.assertEquals(images, retrievedImages);
+  }
+
+  @Test
+  public void testGetImagesNull() {
+    // Create a theme with a null list of images
+    Theme theme = new Theme(Color.WHITE, Color.BLACK, "Arial", null);
+
+    // Retrieve the images from the theme
+    List<Image> retrievedImages = theme.getImages();
+
+    // Assert that the retrieved images are an empty list
+    Assertions.assertEquals(Collections.emptyList(), retrievedImages);
+  }
+
+
+  @Test
+  public void testSetImagesThemeBuilder() {
+    // Create a list of images
+    List<Image> images = new ArrayList<>();
+    images.add(new Image("L.png"));
+    images.add(new Image("Loveball.png"));
+
+    // Create a theme using the ThemeBuilder
+    Theme theme = new Theme.ThemeBuilder()
+        .setImages(images)
+        .build();
+
+    // Retrieve the images from the theme
+    List<Image> retrievedImages = theme.getImages();
+
+    // Assert that the retrieved images match the original images
+    Assertions.assertEquals(images, retrievedImages);
+  }
+
 
 }
